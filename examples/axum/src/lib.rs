7e6985974ea5487656b6f5e71bdc8ba834554ced
Alexander van Saase <avsaase@gmail.com>
2024-03-12 21:40:59 +0100

    Try to get it working

--- examples/axum/src/lib.rs
+++ examples/axum/src/lib.rs
@@ -8,25 +8,25 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use tower_service::Service;
-use worker::{event, kv::KvStore, Context, Env, Request, Response};
+use worker::{event, kv::KvStore, Context, Env, HttpRequest, Request, Response};
 
 #[event(fetch)]
 async fn fetch(req: Request, env: Env, _ctx: Context) -> worker::Result<Response> {
     console_error_panic_hook::set_once();
 
+    let http_request = HttpRequest::from(req);
+
     let response = Router::new()
         .route("/", get(root))
         .route("/users", post(create_user))
-        .with_state(AppState {
-            kv: Arc::new(env.kv("KV").unwrap()),
-        })
-        .call(req)
+        // .with_state(AppState {
+        //     kv: Arc::new(env.kv("KV").unwrap()),
+        // })
+        .call(http_request)
         .await
         .unwrap();
 
-    // Ok(response.map(Body::new))
-
-    todo!()
+    Ok(response.into())
 }
 
 pub async fn root() -> Html<&'static str> {
